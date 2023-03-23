40c2abf35883b60e2ffa119ca6a8bf8fc6d430df
Ederon <Edleron@hotmail.com>
2023-03-23 23:24:22 +0300

    add : pixi starting feature

---
+++ src/scripts/App.js
@@ -0,0 +1,22 @@
+import * as PIXI from "pixi.js";
+import { Loader } from "./Loader";
+import { MainScene } from "./MainScene";
+
+export class App {
+    run() {
+        // Create Canvas
+        this.app = new PIXI.Application({ resizeTo: window });
+        document.body.appendChild(this.app.view);
+        console.log(this.app);
+
+        // Loader Resources
+        this.loader = new Loader(this.app.loader);
+        this.loader.preLoad().then(() => this.start());
+    }
+
+    start() {
+        console.log("The game started");
+        this.scene = new MainScene();
+        this.app.stage.addChild(this.scene.container);
+    }
+}
