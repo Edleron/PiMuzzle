1087e1ca5f384272279ce448b5cb1137e4685170
Ederon <Edleron@hotmail.com>
2023-04-02 22:59:13 +0300

    fix : main scene

--- src/scripts/MainScene.js
+++ src/scripts/MainScene.js
@@ -1,17 +1,24 @@
 import * as PIXI from "pixi.js";
 import { Globals } from "./Globals";
+import { PuzzleGrid } from "./PuzzleGrid";
 
 export class MainScene {
     constructor() {
         this.container = new PIXI.Container();
         this.createBackground();
+        this.createPuzzleGrid();
     }
 
     createBackground() {
         console.log(Globals);
         this.background = new PIXI.Sprite(Globals.resources["background"].texture);
-        this.container.addChild(this.background);
         this.background.width = window.innerWidth;
         this.background.height = window.innerHeight;
+        this.container.addChild(this.background);
+    }
+
+    createPuzzleGrid() {
+        const grid = new PuzzleGrid();
+        this.container.addChild(grid.container);
     }
 }
